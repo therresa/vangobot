c51422acdf2c75541cba0857d78ec8814bc25098
Theresa Nguyen <74863316+therresa@users.noreply.github.com>
2022-11-21 18:26:13 -0500

    added basic comments

--- VanGoBot.c
+++ VanGoBot.c
@@ -1,4 +1,9 @@
-
+/*
+Group members: Eric Aleong, Owen Leather, Nush Majra, Theresa Nguyen
+Group 4-17
+Version: 1.0
+Description: Functions and main written in ROBOTC for VanGoBot. 
+*/
 task main()
 {
 
