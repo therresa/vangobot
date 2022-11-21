7505ccb66205b7746f2f8016c848d5ea0c8faa9e
Theresa Nguyen <74863316+therresa@users.noreply.github.com>
2022-11-21 18:55:28 -0500

    Update VanGoBot.c
    added function prototypes and constants, added sensor config

--- VanGoBot.c
+++ VanGoBot.c
@@ -5,9 +5,39 @@ Version: 1.0
 Description: Functions written in ROBOTC for VanGoBot to be deployed
 on EV3 brick.
 */
+
+//Global constants
+const float ENCODER_TO_INCH = 0, PEN_UP = 0, PEN_DOWN = 0, GANTRY_KP = 0,
+PEN_KP = 0, Y_AXIS_HOME_DISTANCE = 0;
+
+//MotorCommand struct
+struct MotorCommand {
+    float x;
+    float y;
+    bool liftPen;
+}
+
+//Prototypes
+void movePen(int xPower, int yPower);
+void autoMovePen(float x, float y);
+void liftLowerPen(bool lifted);
+void home();
+MotorCommand readNextCommand(TFileHandle &fileIn);
+void manualMove();
+void automaticMode(TFileHandle &fileIn, float x, float y, float size);
+void automaticModeMenu();
+void mainMenu();
+void configureSensors();
+
 task main()
 {
 
+}
 
-
+void configureSensors()
+{
+	SensorType[S1] = sensorEV3_Touch;
+  wait1Msec(50);
+  SensorType[S2] = sensorEV3_Ultrasonic;
+  wait1Msec(50);
 }
